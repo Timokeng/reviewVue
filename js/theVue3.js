be31ae9b44bc9ff3c915261aca39672890f70bfa
Timoking <13466083337@163.com>
2020-02-24 11:20:03 +0800

    review vue about how to set dynamic class and style

---
+++ js/theVue3.js
@@ -0,0 +1,23 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        isActive: true,
+        errorClass: false,
+        dynamicClass: {
+            active: true,
+            errorClass: false
+        },
+        styleData: {
+            color: "red",
+            fontSize: "18px"
+        }
+    },
+    computed: {
+        computedClass: function(){
+            return {
+                active: this.isActive && !this.errorClass,
+                errorClass: this.errorClass
+            }
+        }
+    }
+})
