81b0bf82d33e3780a5935eeaa8b78aaf1d564d65
Timoking <13466083337@163.com>
2021-05-10 16:25:58 +0800

    reviewVue 2021_5_10

---
+++ vue-cli/data2021_5_10/example-1/src/components/ShowBox.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="show-box">
+        <div class="title">{{msg.title}}</div>
+        <div class="article">{{msg.article}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ShowBox',
+    props: {
+        msg: Object
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.title {
+    margin: 20px;
+    font-size: 36px;
+    font-weight: 600;
+}
+
+.article{
+    font-size: 20px;
+}
+</style>
---
+++ vue-cli/data2021_5_10/example-1/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <ShowBox :msg='msg'/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ShowBox from '@/components/ShowBox.vue'
+
+export default {
+  name: 'Home',
+  data: function(){
+    return {
+      msg: {
+        title: 'Welcome to My Project',
+        article: 'This project is used to show message about me'
+      }
+    }
+  },
+  components: {
+    ShowBox
+  }
+}
+</script>
