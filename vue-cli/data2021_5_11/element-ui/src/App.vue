3bc38c313e83ac0d0a82589d225c0b6a16799629
Timoking <13466083337@163.com>
2021-05-11 17:36:25 +0800

    reviewVue 2021_5_11

---
+++ vue-cli/data2021_5_11/element-ui/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <div class="show">
+      <el-row :gutter="10">
+        <el-col :span="12"><div class="example-1"></div></el-col>
+        <el-col :span="12"><div class="example-1"></div></el-col>
+      </el-row>
+      <el-row :gutter="10">
+        <el-col :span="6"><div class="example-1"></div></el-col>
+        <el-col :span="6"><div class="example-1"></div></el-col>
+        <el-col :span="6" :offset="6"><div class="example-1"></div></el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.el-row {
+  margin-bottom: 10px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+
+.show{
+  padding: 10px;
+  background-color: blue;
+}
+
+.example-1 {
+  min-height: 50px;
+  background-color: blueviolet;
+  border: black 1px solid;
+  border-radius: 5px;
+}
+
+</style>
